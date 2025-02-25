233cbc444937d1335ea5e6608bf79f6aa4d31f18
Mint514 <tomhar94@gmail.com>
2025-02-25 17:57:59 +0000

    update site URL in docusaurus configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mint514.github.io',
+  url: 'https://github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/casper-docs/',
@@ -25,7 +25,7 @@ const config = {
   organizationName: 'Mint514', // Usually your GitHub org/user name.
   projectName: 'casper-docs', // Usually your repo name.
   trailingSlash: false,
-  
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
